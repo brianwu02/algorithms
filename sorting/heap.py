97aa612b5de9f9a9d10268f5fe4b13318d8c1ab6
Brian Wu <brianwu02@gmail.com>
2014-03-15 16:57:13 -0400

    more rename & refactor methods

--- sorting/heap.py
+++ sorting/heap.py
@@ -13,10 +13,11 @@ class Heap(object):
         # initialize the heap with dummy variables
         #self.heap = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
         #self.data = [5, 4, 9, 7, 19, 8, 17, 2, 6, 5, 21]
-        self.data = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
-        self.heap_size = len(self.data) - 1
-        self.heap_length = len(self.data)
-        print(self.data)
+        self.A = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
+        self.heap_size = len(self.A) - 1
+        # this attribute does not get modified.
+        self.heap_length = len(self.A) - 1
+        print("unsorted list: %s") % (self.A)
         pass
 
     def get_parent(self, i):
@@ -32,21 +33,23 @@ class Heap(object):
         return 2 * (i + 1)
 
     def max_heapify(self, i):
-        """runs in O(lg n) time. maintains the max-heap property"""
-        A = self.data
+        """runs in O(lg n) time. maintains the max-heap property. 
+        compares the children of i'th node and assures that the 
+        value of the parent node is larger than both child nodes.
+        """
         left = self.left(i)
         right = self.right(i)
-        if left <= self.heap_size and A[left] > A[i]:
+        if left <= self.heap_size and self.A[left] > self.A[i]:
             largest = left
         else:
             largest = i
 
-        if right <= self.heap_size and A[right] > A[largest]:
+        if right <= self.heap_size and self.A[right] > self.A[largest]:
             largest = right
 
         if largest != i:
-            # swap A[i] and A[largest] then recursively call max_heapify down the node
-            A[i], A[largest] = A[largest], A[i]
+            # swap A[i] and A[largest] then recursively call max_heapify down the node 
+            self.swap(i, largest)
             self.max_heapify(largest)
 
     def build_max_heap(self):
@@ -55,26 +58,22 @@ class Heap(object):
         Output: A => modified to represent a heap 
         running time: O(n) where n = len(A)
         """
-        A = self.data
         for i in range(self.heap_size / 2, -1, -1):
             self.max_heapify(i)
-        print self.data
+        print("max_heap: %s") % (self.A)
 
     def heap_sort(self):
         """runs in O(n lg n) sorts an array in place. """
         self.build_max_heap()
-        A = self.data
-        for i in range(len(A)-1, 0, -1):
-            print i
-            #A[0], A[i] = A[i], A[0]
+        heap_length = self.heap_length
+        for i in range(heap_length, 0, -1):
             self.swap(0, i)
             self.heap_size -= 1
             self.max_heapify(0)
-        print self.data
+        print ("sorted heap %s") % (self.A)
 
     def swap(self, i, j):
-        A = self.data
-        A[i], A[j] = A[j], A[i]
+        self.A[i], self.A[j] = self.A[j], self.A[i]
 
     def max_heap_insert(self):
         pass
