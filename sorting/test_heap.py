3e7744e988b83806c2432fa8d8ad912b70057322
Brian Wu <brianwu02@gmail.com>
2014-04-12 14:32:17 -0400

    changed from class variable to local variable.

--- sorting/test_heap.py
+++ sorting/test_heap.py
@@ -9,27 +9,33 @@ class TestCase(unittest.TestCase):
 
     def setUp(self):
         """instantiate Heap object"""
-        #self.h = Heap()
+        self.unsorted_array = [4, 10, 12, 1, 5, 8, 7, 9, 1, 25, 2, 3]
         pass
 
     def tearDown(self):
         """don't think anything needs to be removed."""
         pass
 
+    def test_sort_does_not_work(self):
+        """makes sure that heap does not return a sorted array before
+        sort is called. sounds stupid but needs to be done lol."""
+
+
+
     def test_sort_correctness(self):
         """tests to see whether or not Heap sorts correctly by sorting
         a list using heap sort and comparing it to a sort using python's
         standard library sort."""
-        unsorted_array = [4, 10, 12, 1, 5, 8, 7, 9, 1, 25, 2, 3]
+        
 
         # initialize heap with this array
-        self.h = Heap(unsorted_array)
+        h = Heap(self.unsorted_array)
         # build the heap using max heapify
-        self.h.build_max_heap()
+        h.build_max_heap()
         # sort the array using heap sort 
-        self.h.heap_sort()
-        list1 = self.h.get_array()
-        list2 = sorted(unsorted_array)
+        h.heap_sort()
+        list1 = h.get_array()
+        list2 = sorted(self.unsorted_array)
         self.assertEqual(list1, list2)
 
 
